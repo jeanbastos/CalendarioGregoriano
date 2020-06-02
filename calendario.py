dfeafab91e5e8b06100e09aacafc1644cac28ac6
Jean Carlo Bastos <paniagua.b.jean@gmail.com>
2020-06-01 20:06:14 -0600

    Archivo inicial

---
+++ calendario.py
@@ -0,0 +1,7 @@
+
+dias =  {‘1’ : 31, ’2’ : 28, ‘3’ : 31 , ‘4’ : 30 , ‘5’ : 31 , ‘6’ : 30 , ‘7’ : 31 , ‘8’ : 31, , ‘9’ : 30 , ‘10’ : 31} , ‘11’ : 30 , ‘12’ : 31
+
+def fecha_es_tupla(dia, mes, anho):
+    if(mes < 1 or mes > 12):
+        return False
+    return True
